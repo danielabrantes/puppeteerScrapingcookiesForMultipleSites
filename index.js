d10a12a2ec6de571323f3b998241c1857873c5f5
danielabrantes <daniel.abrantes@gmail.com>
2019-04-10 22:04:58 +0100

    Promise.all instead of foreach or for...

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ const puppeteer = require("puppeteer");
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  let site = [
-    'https://www.google.com',
-    'https://www.youtube.com',
-    'https://www.facebook.com',
-    'https://www.google.pt',
-    'https://www.sapo.pt',
-    'https://www.wikipedia.org',
-    'https://www.olx.pt',
-    'https://www.instagram.com',
-    'https://www.abola.pt'
+  const sites = [
+    "https://www.google.com",
+    "https://www.youtube.com",
+    "https://www.facebook.com",
+    "https://www.google.pt",
+    "https://www.sapo.pt",
+    "https://www.wikipedia.org",
+    "https://www.olx.pt",
+    "https://www.instagram.com",
+    "https://www.abola.pt"
   ];
-  for (let i = 1; i < site.length; i++) {
-    await page.goto(site[i]);
-    let cookies = await page.cookies();
-    console.log(site[i]);
-    console.log(cookies);
-    console.log("");
-  }
+  await Promise.all(
+    sites.map(async site => {
+      await page.goto(site);
+      let cookies = await page.cookies();
+      console.log(site);
+      console.log(cookies);
+      console.log("");
+    })
+  );
   browser.close();
 }
 run();
