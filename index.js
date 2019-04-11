891010368bc8fa4b85a6618e62ac5170b7e131a7
Pedro Daniel Gaspar Abrantes <daniel.abrantes@gmail.com>
2019-04-11 16:14:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const puppeteer = require("puppeteer");
-async function run() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const sites = [
-    "https://www.google.com",
-    "https://www.youtube.com",
-    "https://www.facebook.com",
-    "https://www.google.pt",
-    "https://www.sapo.pt",
-    "https://www.wikipedia.org",
-    "https://www.olx.pt",
-    "https://www.instagram.com",
-    "https://www.abola.pt"
-  ];
-  await Promise.all(
-    sites.map(async site => {
-      await page.goto(site);
-      let cookies = await page.cookies();
-      console.log(site);
-      console.log(cookies);
-      console.log("");
-    })
-  );
-  browser.close();
-}
-run();
+const puppeteer = require('puppeteer');
+(async () => {
+    /*
+    // a trick to run headless with GPU enabled.
+    //const browser = await puppeteer.launch({headless: false,args: ['--headless']});
+    */
+    const browser = await puppeteer.launch({headless: true});
+    const sites = [
+      "https://www.google.com",
+      "https://www.youtube.com",
+      "https://www.facebook.com",
+      "https://www.google.pt",
+      "https://www.sapo.pt",
+      "https://www.wikipedia.org",
+      "https://www.olx.pt",
+      "https://www.instagram.com",
+      "https://www.abola.pt"
+    ];
+    await Promise.all(
+      sites.map(async site => {
+        const context = await browser.createIncognitoBrowserContext();
+        const page = await context.newPage();
+        await page.goto(site, {waitUntil: 'networkidle2'});
+        let cookies = await page._client.send('Network.getAllCookies');
+        await console.log(cookies.cookies);
+        await page.deleteCookie();
+        await page.close();
+      })
+    );
+    await browser.close();
+})();
